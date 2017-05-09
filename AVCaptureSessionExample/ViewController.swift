1b75b5ebbd69f22e288752711b4a4c9029f546a2
Nick Wilkerson <nickwilkerson@gmail.com>
2017-05-09 14:56:38 -0700

    initial commit

--- AVCaptureSessionExample/ViewController.swift
+++ AVCaptureSessionExample/ViewController.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
+import AVFoundation
+
+
 
 class ViewController: UIViewController {
-
+    var captureSession = AVCaptureSession();
+    var sessionOutput = AVCapturePhotoOutput();
+    var sessionOutputSetting = AVCapturePhotoSettings(format: [AVVideoCodecKey:AVVideoCodecJPEG]);
+    var previewLayer = AVCaptureVideoPreviewLayer();
+    var cameraView: UIView?
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        cameraView = UIView(frame: view.frame)
+        view.addSubview(cameraView!)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let deviceDiscoverySession = AVCaptureDeviceDiscoverySession(deviceTypes: [AVCaptureDeviceType.builtInDualCamera, AVCaptureDeviceType.builtInTelephotoCamera,AVCaptureDeviceType.builtInWideAngleCamera], mediaType: AVMediaTypeVideo, position: AVCaptureDevicePosition.unspecified)
+        for device in (deviceDiscoverySession?.devices)! {
+            if(device.position == AVCaptureDevicePosition.back){
+                do{
+                    let input = try AVCaptureDeviceInput(device: device)
+                    if(captureSession.canAddInput(input)){
+                        captureSession.addInput(input);
+                        
+                        if(captureSession.canAddOutput(sessionOutput)){
+                            captureSession.addOutput(sessionOutput);
+                            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession);
+                            previewLayer.frame = (cameraView?.bounds)!
+                            previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+                            previewLayer.connection.videoOrientation = AVCaptureVideoOrientation.portrait;
+                            cameraView?.layer.addSublayer(previewLayer);
+                        }
+                    }
+                }
+                catch{
+                    print("exception!");
+                }
+            }
+        }
+
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        captureSession.startRunning()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override var shouldAutorotate: Bool {
+        return false
     }
-
-
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
+    
 }
-
